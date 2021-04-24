dbb84cf7b8417f507359a6206b992a3e475aaa76
Hardik-Sihmar <83064171+Hardik-Sihmar@users.noreply.github.com>
2021-04-24 13:55:35 +0530

    Add files via upload

---
+++ fruitslist.py
@@ -0,0 +1,8 @@
+f1 = input("Enter teh name of first fruit: ")
+f2 = input("Enter the name of second fruit: ")
+f3 = input("Enter the name of third fruit: ")
+f4 = input("Enter the name of fourth fruit: ")
+
+allfruits = [f1, f2, f3, f4]
+
+print(allfruits)
